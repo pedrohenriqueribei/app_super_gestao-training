028c72e53bb75830d780c7935bfdb7ef8c7fb1ba
Pedro Henrique <pedrohenriqueribei@gmail.com>
2025-04-19 10:39:30 -0300

    Eager Loading de produto com detalhes

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
+use App\Models\ProdutoDetalhe;
 use App\Models\Unidade;
 use Illuminate\Http\Request;
 use PHPUnit\TextUI\Configuration\Constant;
@@ -38,7 +39,7 @@ class ProdutoController extends Controller
     public function index(Request $request)
     {
         //aula 164
-        $produtos = Produto::paginate(15);
+        $produtos = Produto::with(['ProdutoDetalhe'])->paginate(15);
         
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all()]);
     }
