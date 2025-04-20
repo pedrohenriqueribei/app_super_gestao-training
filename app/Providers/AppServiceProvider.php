63782ef8d8508db606cef7e7584fecdb97e7b4a1
Pedro Henrique <pedrohenriqueribei@gmail.com>
2025-04-20 11:55:57 -0300

    Alterado o Fuso Horário do Projeto Laravel e do Banco de Dados para Horário Oficial de Brasília

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Define o timezone da conexão MySQL como São Paulo
+        DB::statement("SET time_zone = '-03:00'");
     }
 }
